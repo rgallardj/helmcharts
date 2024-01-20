628e054dec835ea78b2e3d50a1746fc39c150edc
rgallardj <rgallardj@alumnos.unex.es>
2024-01-20 19:24:46 +0100

    updating helm chart

---
+++ eurekaserver/src/test/java/es/unex/aos/eureka/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unex.aos.eureka.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
